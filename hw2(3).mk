6506c2eac3ecbf718014bcd06647e4af6bab7e19
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2019-05-11 08:54:22 -0500

    Update hw2(3).mk

--- hw2(3).mk
+++ hw2(3).mk
@@ -1,19 +1,19 @@
-Resultados_hw2.pdf: resultados_hw2.tex 
+Resultados_hw2.pdf: Resultados_hw2.tex 
     pdflatex Resultados_hw2.tex
 
-resultados_hw2.tex: Plots_hw2.o 
+Resultados_hw2.tex: Plots_hw2.py datos1.txt Fourier.o
     python3 Plots_hw2.py
 
-Plots_hw2.o: Edificio.o
-    pyhton3 Plots_hw2.py
-
-Edificio.o: Edificio.cpp
+datos1.txt: Edificio.cpp
     g++ Edificio.cpp -o Edificio.out
     ./Edificio.out
+
+Fourier.o : Fourier.py
+    python3 Fourier.py
     
     
 clean:
     rm *.out
     rm *.txt
     rm *.pdf
-    
+    rm *.o
