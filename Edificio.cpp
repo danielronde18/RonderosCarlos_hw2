75e46d607bd7dd0a517de693d2b7543c6229b8c8
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2019-05-10 22:06:36 -0500

    Update Edificio.cpp

--- Edificio.cpp
+++ Edificio.cpp
@@ -1,21 +1,26 @@
-#include<iostream>
-#include<math.h>
-#include <stdio.h>
+#include <cmath>
+#include <vector>
 #include <fstream>
-
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-double g=10;
-double c=0.2;
-double m=1000;
-double gamma=0.0;
-double K=2000;
-double omega=1*pow(k/m,0.5);
+double dt = 0.01;
+double g = 0.0;
+double k = 2000.0;
+double m = 1000.0;
 
+double eq1( double );
+double eq2( double );
+double eq3( double );
+double eq4( double, double, double, double, double );
+double eq5( double, double, double, double );
+double eq6( double, double, double );
+void resolver( double, double, double&, double&, double&, double&, double&, double& );
+void condiciones_iniciales( double&, double&, double&, double&, double&, double& );
 
-
-int main(void){
+int main(){
     
     
     
