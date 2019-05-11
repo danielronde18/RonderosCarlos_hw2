104af65a6d509a6a3eb10058db7d30d40ea0c13c
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2019-05-10 22:01:27 -0500

    Add files via upload

---
+++ Edificio.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+#include <stdio.h>
+#include <fstream>
+
+
+using namespace std;
+
+double g=10;
+double c=0.2;
+double m=1000;
+double gamma=0.0;
+double K=2000;
+double omega=1*pow(k/m,0.5);
+
+
+
+int main(void){
+    
+    
+    
+    
+    
+    
+ return 0;   
+}
