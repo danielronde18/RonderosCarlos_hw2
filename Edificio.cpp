dc9134b99d56b0dbff98755abfa893b62c69fd8f
danielronde18 <42391468+danielronde18@users.noreply.github.com>
2019-05-11 09:43:47 -0500

    Update Edificio.cpp

--- Edificio.cpp
+++ Edificio.cpp
@@ -25,6 +25,98 @@ int main(){
 	double w = sqrt(k/m);
 	double u1, u2, u3, v1, v2, v3;
 	condiciones_iniciales( u1, u2, u3, v1, v2, v3);
+	
+	
+		ofstream info1;
+	info1.open("uiVSw.txt");
+	for( double t = 0.0; t < 150; t = t + dt ){
+		resolver( w, t, u1, u2, u3, v1, v2, v3 );
+		info1 << t << "\t" << u1 << "\t" << u2 << "\t" << u3 << "\t" << v1 << "\t" << v2 << "\t" << v3 << endl;
+	}
+	info1.close();
+
+	w = 0.63;
+	condiciones_iniciales( u1, u2, u3, v1, v2, v3);
+	ofstream info2;
+	info2.open("datos1.txt");
+	for( double t = 0.0; t < 150; t = t + dt ){
+		resolver( w, t, u1, u2, u3, v1, v2, v3 );
+		info2 << t << "\t" << u1 << "\t" << u2 << "\t" << u3 << "\t" << v1 << "\t" << v2 << "\t" << v3 << endl;
+	}
+	info2.close();
+
+	w = 1.30;
+	condiciones_iniciales( u1, u2, u3, v1, v2, v3);
+	ofstream info3;
+	info3.open("datos2.txt");
+	for( double t = 0.0; t < 150; t = t + dt ){
+		resolver( w, t, u1, u2, u3, v1, v2, v3 );
+		info3 << t << "\t" << u1 << "\t" << u2 << "\t" << u3 << "\t" << v1 << "\t" << v2 << "\t" << v3 << endl;
+	}
+	info3.close();
+
+	w = 2.50;
+	condiciones_iniciales( u1, u2, u3, v1, v2, v3);
+	ofstream info4;
+	info4.open("datos3.txt");
+	for( double t = 0.0; t < 150; t = t + dt ){
+		resolver( w, t, u1, u2, u3, v1, v2, v3 );
+		info4 << t << "\t" << u1 << "\t" << u2 << "\t" << u3 << "\t" << v1 << "\t" << v2 << "\t" << v3 << endl;
+	}
+	info4.close();
+
+	w = 4.0;
+	condiciones_iniciales( u1, u2, u3, v1, v2, v3);
+	ofstream info5;
+	info5.open("datos4.txt");
+	for( double t = 0.0; t < 150; t = t + dt ){
+		resolver( w, t, u1, u2, u3, v1, v2, v3 );
+		info5 << t << "\t" << u1 << "\t" << u2 << "\t" << u3 << "\t" << v1 << "\t" << v2 << "\t" << v3 << endl;
+	}
+	info5.close();
+
+	int N = 100;
+	double tf = 150.0;
+	int arrtam = int(tf/dt);
+	double w0 = 0.2*sqrt(k/m);
+	double w1 = 3.0*sqrt(k/m);
+	double dw = ( w1 - w0 )/(N-1);
+	double W[N], T[arrtam];
+	
+	ofstream info6;
+	info6.open("ui_maximo.txt");
+
+	for( int i = 0; i<N; i++ ){
+		W[i] = w0 + dw*i;
+	}
+
+	for( int i = 0; i<arrtam; i++ ){
+		T[i] = i*dt;
+	}
+	
+	for( int i=0; i < N; i++){
+		condiciones_iniciales( u1, u2, u3, v1, v2, v3);
+		vector<double> u1_t(arrtam), u2_t(arrtam), u3_t(arrtam);
+
+		for( int j=0; j < arrtam; j++ ){
+			resolver( W[i], T[j], u1, u2, u3, v1, v2, v3 );
+			u1_t[j] = u1;
+			u2_t[j] = u2;
+			u3_t[j] = u3;
+		}
+
+		sort( u1_t.begin(), u1_t.end() );
+		sort( u2_t.begin(), u2_t.end() );
+		sort( u3_t.begin(), u3_t.end() );
+
+		double u1_max = u1_t[arrtam-1];
+		double u2_max = u2_t[arrtam-1];
+		double u3_max = u3_t[arrtam-1];
+		info6 << W[i] << "\t" << u1_max << "\t" << u2_max << "\t" << u3_max << endl;
+	}
+	
+	info6.close();
+
     
     
     
@@ -43,17 +135,6 @@ void condiciones_iniciales(double& u1, double& u2, double& u3, double& v1, doubl
 
 
 
-double eq4( double t, double u1, double u2, double v1, double w ){
-	return 1/m*(-g*v1 + k*(u2 - 2*u1) + sin( w*t));
-}
-
-double eq5( double u1, double u2, double u3, double v2 ){
-	return 1/m*(-g*v2 + k*(u1 - 2*u2 + u3) );
-}
-
-double eq6( double u2, double u3, double v3 ){
-	return 1/m*(-g*v3 + k*(u2 - u3));
-}
 
 
 double eq1( double v1 ){
@@ -67,6 +148,21 @@ double eq2( double v2 ){
 double eq3( double v3 ){
 	return v3;
 }
+
+
+
+double eq4( double t, double u1, double u2, double v1, double w ){
+	return 1/m*(-g*v1 + k*(u2 - 2*u1) + sin( w*t));
+}
+
+double eq5( double u1, double u2, double u3, double v2 ){
+	return 1/m*(-g*v2 + k*(u1 - 2*u2 + u3) );
+}
+
+double eq6( double u2, double u3, double v3 ){
+	return 1/m*(-g*v3 + k*(u2 - u3));
+}
+
 
 void resolver( double w, double t, double &u1, double &u2, double &u3, double &v1, double &v2, double &v3){
 	double iter00, iter01, iter02, iter03, iter04, iter05;
